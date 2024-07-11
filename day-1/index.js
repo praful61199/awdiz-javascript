732b58ada70914ebf0cc2a8b899595b8d0eec52e
praful61199 <110277893+praful61199@users.noreply.github.com>
2024-07-12 00:38:13 +0530

    day-1

---
+++ day-1/index.js
@@ -0,0 +1,47 @@
+// number = 10 ;
+// console.log(number)
+// var number;    //  hoisting 
+// let number;             // cant access number
+
+// console.log(20<40);
+// console.log(40>20);
+// console.log(30>=40);
+// console.log(30<=40);
+// console.log(30<=30);
+
+
+// console.log(4==4)
+// console.log(3=="3")
+// console.log(5==="5")
+
+
+
+// if else statment
+
+if(10<20){
+    console.log("if  block executed")
+}else{
+    console.log("else block executed")
+}
+
+
+
+
+
+// if (10 < 20) {
+//     console.log("inside if");
+//   } else {
+//     console.log("inside else");
+//   }
+
+
+
+
+//   var age1=20;
+//   var age2=40;
+
+//   if(age1>age2){
+//     console.log("number 1 is greater than number 2")
+//   }else{
+//     console.log("lesser than num 2")
+//   }
