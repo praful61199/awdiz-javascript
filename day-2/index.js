ec808d576c1a1114e30429719da983ff50c74fc4
praful61199 <110277893+praful61199@users.noreply.github.com>
2024-07-13 19:05:35 +0530

    day -2

---
+++ day-2/index.js
@@ -0,0 +1,96 @@
+// if elseif statement
+
+let password = 35
+
+if(password>50){
+console.log("password is greater than 50");
+}
+else if(password==100){
+    console.log("password is 50");
+}
+else{
+console.log("password is 35");
+}
+
+
+// var age = 18; // 17 // 20
+
+// if (age < 18) {
+//   console.log("You are not eligible for Driving license.");
+// } else if (age == 18) {
+//   console.log("You are eligible for Learning license.");
+// } else {
+//   console.log("You are eligible for Driving license.");
+// }
+
+
+
+
+
+// switch case
+
+// switch(data){
+//     case 1 :
+//         log()
+//         break;
+//     case 2 :
+//         log()
+//         break;
+//     case 3 :
+//         log()
+//         break;
+//     default:
+//         log()
+// }
+
+
+var userselectedgame="fifa"
+
+switch(userselectedgame){
+    case "mario":
+    console.log("user has selected mario")
+    break;
+    case "contra":
+    console.log("user has selected contra")
+    break;
+    case "fifa":
+    console.log("user has selected fifa")
+    break;
+    default:
+        console.log("user has not selected any option")
+
+}
+
+
+
+// Logical Operators
+
+// && -> and  operator
+// if all conditions are true then only it'll return true
+// if any single condition is false then it'll return false
+// || -> or operator
+// if any single condition is true then it'll return true
+// if all condition are false then only it'll return  false
+
+
+
+// logical operators
+// and --> &&
+// || --> or
+
+// var student1 = 100;
+// var student2 = 50;
+// var student3 = 20;
+
+// console.log(student1 >= 50 && student2 >= 50 && student3 >= 50);
+
+
+
+var fruit1 = 60;
+var fruit2 = 70;
+var fruit3 = 80;
+
+ console.log(fruit1==60 && fruit2==80 && fruit3== 90)
+
+console.log(fruit1 == 60 || fruit2==80|| fruit3 ==100)
+
