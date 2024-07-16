2b9b6461bbec3f1c5a1f925b2644b720840bf952
praful61199 <110277893+praful61199@users.noreply.github.com>
2024-07-16 17:07:52 +0530

    day-4

---
+++ day-4/index.js
@@ -0,0 +1,60 @@
+// arrow function >>> syntax
+
+// const username = () => {
+//     console.log("hello")
+// }
+// username()
+
+// program to check number is even or odd using arrow function
+
+// const checkEvenOdd= (number) => {
+
+//     if(number % 2  == 0){
+//         console.log(`given number ${number} is even`);
+//     }else{
+//         console.log(`given number ${number} is odd `);
+//     }
+// }
+// checkEvenOdd(0);
+
+
+// for loops  
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i); // 1 2 3 ... 10
+//     // i++ / 1 => 2 => 3 => 4 .. 10 => 11
+//   }
+
+// // new 
+// var b =5
+
+// for (b ; b<=10 ; b++){
+//     console.log(b)
+// }
+
+// print numbers from 20 to 40 
+
+// for (i=20 ; i<=40 ;i++){
+//     console.log(i)
+// }
+
+// print numbers from  1 to 100  condition is all must be odd
+
+// for (j=1 ;j<=100;j+=2){     // new concept i+=2 it will add 2 more same i+=5 it will add more 5
+//     console.log(j)
+
+// }
+
+// Print numbers from 50 -150, 50, 60, 70, 80, ... 150
+
+// for(k=50; k<=150 ; k+=10){
+//     console.log(k)
+    
+// }
+
+for(let i=1 ; i<=5 ; i++){
+
+    for(let j=1;j<=i ;j++){
+        console.log("*")
+    }
+}
